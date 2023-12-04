24b423e0213adb3c4abd64eefbeee09e10719b89
John Handley <jwhandley17@gmail.com>
2023-12-04 13:56:42 +0000

    Make the code more idiomatic

--- src/Day4.java
+++ src/Day4.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day4 {
     public static void part1() throws IOException {
@@ -12,40 +13,26 @@ public class Day4 {
             HashSet<Integer> winningNumbers = getNumberSet(game[0]);
             List<Integer> card = getNumberList(game[1]);
             int score = calculateCardScore(card, winningNumbers);
-            result += (int) Math.pow(2, (score-1));
-            // System.out.println(STR."Winning numbers: \{winningNumbers}, Card: \{card}, Score: \{score}");
+            result += 1 << (score - 1);
         }
 
         System.out.println(STR."Result for part 1: \{result}");
     }
 
     static HashSet<Integer> getNumberSet(String input) {
-        HashSet<Integer> numSet = new HashSet<>();
-        Arrays.stream(input.trim().split("\\s+")).forEach(i -> {
-            int num = Integer.parseInt(i);
-            numSet.add(num);
-        });
-        return numSet;
+        return Arrays.stream(input.trim().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(HashSet::new));
     }
 
     static List<Integer> getNumberList(String input) {
-        List<Integer> numList = new ArrayList<>();
-        Arrays.stream(input.trim().split("\\s+")).forEach(i -> {
-            int num = Integer.parseInt(i);
-            numList.add(num);
-        });
-        return numList;
+        return Arrays.stream(input.trim().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     static int calculateCardScore(List<Integer> card, HashSet<Integer> winningNumbers) {
-        int result = 0;
-        for (Integer number : card) {
-            if (winningNumbers.contains(number)) {
-                result++;
-            }
-        }
-
-        return result;
+        return (int) card.stream().filter(winningNumbers::contains).count();
     }
 
     public static void part2() throws IOException {
@@ -53,29 +40,26 @@ public class Day4 {
         HashMap<Integer, Integer> copyCount = new HashMap<>();
         int i = 0;
         for (String line: lines) {
-            int numCopies;
-            if (!copyCount.containsKey(i)) {
-                numCopies = 1;
-                copyCount.put(i, numCopies);
-            } else {
-                numCopies = copyCount.get(i);
-            }
-
+            int numCopies = copyCount.getOrDefault(i, 1);
+            if (numCopies == 1) copyCount.put(i, 1);
 
             String[] game = line.split(": ")[1].split(" \\| ");
             HashSet<Integer> winningNumbers = getNumberSet(game[0]);
             List<Integer> card = getNumberList(game[1]);
             int score = calculateCardScore(card, winningNumbers);
 
-            for (int k = 0; k < numCopies; k++) {
-                for (int j = 1; j <= score; j++) {
+
+            for (int k = 0; k < numCopies; k++) { // Number of copies we have of card
+                for (int j = 1; j <= score; j++) { // Number of copies we get from card
                     copyCount.put(i+j,copyCount.getOrDefault(i+j, 1)+1);
                 }
             }
+
+
             i++;
         }
 
-        System.out.println(STR."Result for part 1: \{copyCount.values().stream().mapToInt(Integer::intValue).sum()}");
+        System.out.println(STR."Result for part 2: \{copyCount.values().stream().mapToInt(Integer::intValue).sum()}");
 
     }
 }
