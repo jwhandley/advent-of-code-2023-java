005f6a11060ffbe9cb2553fa4592cf959e875db5
John Handley <jwhandley17@gmail.com>
2023-12-08 17:30:08 +0000

    Use more idiomatic expressions

--- src/Day3.java
+++ src/Day3.java
@@ -6,23 +6,13 @@ import java.util.List;
 
 public class Day3 {
     public static void part1() throws IOException {
-        List<String> input = Files.readAllLines(Path.of("Inputs/Day3/input.txt"));
-
-        char[][] charGrid = new char[input.size()][input.get(0).length()];
-
-        for (int i = 0; i < input.size(); i++) {
-            String line = input.get(i);
-            for (int j = 0; j < line.length(); j++) {
-                char c = line.charAt(j);
-                charGrid[i][j] = c;
-            }
-        }
+        char[][] charGrid = buildGrid(Path.of("Inputs/Day3/input.txt"));
 
         StringBuilder numberBuffer = new StringBuilder();
         boolean nearSpecial = false;
         int result = 0;
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = 0; j < input.get(0).length(); j++) {
+        for (int i = 0; i < charGrid.length; i++) {
+            for (int j = 0; j < charGrid[0].length; j++) {
                 char value = charGrid[i][j];
                 if (Character.isDigit(value)) {
                     numberBuffer.append(value);
@@ -71,7 +61,7 @@ public class Day3 {
 
     static char[][] buildGrid(Path inputPath) throws IOException {
         List<String> input = Files.readAllLines(inputPath);
-        char[][] grid = new char[input.size()][input.get(0).length()];
+        char[][] grid = new char[input.size()][input.getFirst().length()];
         for (int i = 0; i < input.size(); i++) {
             String line = input.get(i);
             for (int j = 0; j < line.length(); j++) {
@@ -133,7 +123,7 @@ public class Day3 {
         }
 
 
-        int result = indexCount.values().stream().filter(count -> count.size() == 2).mapToInt(count -> count.get(0) * count.get(1)).sum();
+        int result = indexCount.values().stream().filter(count -> count.size() == 2).mapToInt(count -> count.getFirst() * count.getLast()).sum();
 
         System.out.println(STR. "Result for part 2: \{ result }" );
     }
